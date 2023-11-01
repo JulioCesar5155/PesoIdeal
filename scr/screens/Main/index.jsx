f9956078b2c5decb1c066f8022a9965642f310a5
JulioCesar5155 <125457479+JulioCesar5155@users.noreply.github.com>
2023-11-01 14:00:21 +0000

    Pesoidealgluestack

--- scr/screens/Main/index.jsx
+++ scr/screens/Main/index.jsx
@@ -1,9 +1,66 @@
-import { Box, Text } from "@gluestack-ui/themed";
+import { Box, Text, Center, FormControl, FormControlLabel, FormControlLabelText, Heading, Input, InputField, HStack, Switch, Button, ButtonText } from "@gluestack-ui/themed";
+import { useState } from "react";
 
 
 export default function Main() {
-    
-    return <Box bg="$amber600">
-        <Text>Projeto Gluestacks - Alterado</Text>
+    const [peso, setPeso] = useState("72")
+    const [altura, setAltura] = useState("1.70")
+    const [sexo, setSexo] = useState(false)
+    const [resultado, setResultado] = useState("")
+
+
+    const CalcularHandle = () => {
+        let alturaConvertida = parseFloat(altura)
+        let result = 0
+
+        if (altura>2.2) {
+            setResultado("Altura deve ser menor que 2.2")
+            return;
+        }
+        if (sexo) {
+            result = (62.1*alturaConvertida)-44.7
+            // mulher
+        } else {
+            //homem
+            result = (72.7*alturaConvertida)-58
+        }
+        setResultado(result) 
+    }
+
+    return <Box bg="$amber600" p="$2" h={"$1/2"} w={300} borderRadius={"$md"}>
+        <Center height={"$full"}>
+            <Heading>Peso Ideal</Heading>
+            <FormControl>
+                <FormControlLabel>
+                    <FormControlLabelText>Peso</FormControlLabelText>
+                </FormControlLabel>
+                <Input w={"$full"}>
+                    <InputField value={peso} onChangeText={setPeso} />
+                </Input>
+                <FormControlLabel>
+                    <FormControlLabelText>Altura</FormControlLabelText>
+                </FormControlLabel>
+                <Input w={"$full"}>
+                    <InputField value={altura} onChangeText={setAltura} />
+                </Input>
+                <FormControlLabel>
+                    <FormControlLabelText>Sexo</FormControlLabelText>
+                </FormControlLabel>
+                <Input w={"$full"}>
+                    <InputField/>
+                </Input>
+                <HStack w={"$full"} space="md" justifyContent="center" alignItems="center">
+                <FormControlLabelText>Homem</FormControlLabelText>
+                    <Switch value={sexo} onValueChange={setSexo}/>
+                    <FormControlLabelText>Mulher</FormControlLabelText>
+                </HStack>
+                <Button onPress={CalcularHandle}>
+                    <ButtonText>Calcular</ButtonText>
+                </Button>
+                <Box justifyContent="center" alignItems="center" height={100}>
+                    <Text>Resultado: {resultado}</Text>
+                </Box>
+            </FormControl>
+        </Center>
         </Box>
 }
